e9a930f71a8f6a4ea535ff24850356cb4268bcb5
Bruno MC <brunomc@outlook.com>
2018-11-13 07:35:08 -0200

    sd

--- src/Mainapp.js
+++ src/Mainapp.js
@@ -1,65 +1,85 @@
 import React, { Component } from 'react';
 import './ochain.css';
 import Pf from './estevan.jpg'
-import  { Button } from 'mdbreact';
+import { Button } from 'mdbreact';
 import Hist from './Hist';
+import axios from 'axios';
 
 class Mainapp extends Component {
   constructor(props) {
     super(props);
-    this.state =  {
-      owner_new: { 
-        product_name:'Vitamina A-Z',
-        product_desc: 'Produto original do paraná, com composição: olho de soja, extrato de leite, ovo de codorna destilado. Produto original do paraná, com composição: olho de soja, extrato de leite, ovo de codorna destilado. ',
-        prop_infos: {
-          name:'Estevan Pedro W. R.',
-          user:'estevanpwr',
-          doc:'059.292.829-27',
-          email: 'estevanpwr@outlook.com',
+    this.state = {
+      product: {
+        name: 'Vitamina A-Z',
+        image: '',
+        description: 'Produto original do paraná, com composição: olho de soja, extrato de leite, ovo de codorna destilado. Produto original do paraná, com composição: olho de soja, extrato de leite, ovo de codorna destilado. ',
       },
-    },
-      owner_old: {
-        product_name:'Vitamina A-Z',
-        product_desc: 'Produto original do paraná, com composição: olho de soja, extrato de leite, ovo de codorna destilado. Produto original do paraná, com composição: olho de soja, extrato de leite, ovo de codorna destilado. ',
-        prop_infos: {
-          name:'Bruno Medeiros',
-          user:'brunome',
-          doc:'054.659.361-89',
-          email: 'brunome@outlook.com',
-      },
-    },
-  }
-}
-    render() {
-      return (
-        <div>
-        <div className='greybox z-depth-5 border border-light rounded'>
-
-           <h6 className='text-center' >Produto</h6>
-
-           <h5 className='text-center font-weight-bold '>{this.state.owner_new.product_name}</h5>
-        <hr/>
-            <p>{this.state.owner_new.product_desc}</p>
-        </div>
-
-<div className='greybox z-depth-5 border border-light rounded'>
-
-<h6 className='text-center'>Propriedade</h6>
-<h5 className='text-center font-weight-bold '>{this.state.owner_new.prop_infos.name}</h5>
-
-<div className='text-center'>
-<a><img width="100px" height="100px" src={Pf} alt='profile_photo'  className="rounded-circle" /></a><br/>
-</div>
-<hr/>
-<p1>Usuário: {this.state.owner_new.prop_infos.user}</p1><br/>
-<p1>CNPJ/CPF: {this.state.owner_new.prop_infos.doc}</p1><br/>
-<p1>E-mail: {this.state.owner_new.prop_infos.email}</p1><br/>
-<hr/>
-<div className='text-center'><Button href="/historico" size="sm">Histórico</Button></div>
-</div>
-        </div>
-      );
+      owner: {}
     }
   }
-  
+  async componentWillMount() {
+    var userData;
+    await axios.get('http://localhost/products/' + this.props.match.params.code).then(res => {
+
+      if (res.data.user_id) {
+        axios.get('http://localhost/users/' + res.data.user_id).then(res => {
+          userData = res.data;
+
+          this.setState({
+            owner: {
+              name: userData.username,
+              cpfcnpj: userData.cnpj ? userData.cnpj : userData.cpf,
+              email: userData.email,
+              avatar: userData.avatar,
+              fullname: userData.fullname
+            }
+          });
+        }).catch(res => {
+
+        });
+      }
+      this.setState({
+        product: {
+          name: res.data.name,
+          image: res.data.image,
+          description: res.data.description,
+        }
+      });
+    }).catch(res => {
+      console.log('Produto não cadastrado na base')
+    })
+    console.log(this.state.product);
+  }
+  render() {
+    return (
+      <div>
+        <div style={{ alignItems: 'center', textAlign: 'center' }} className='greybox z-depth-5 border border-light rounded'>
+
+          <h6 className='text-center' >Produto</h6>
+          <img src={"data:image/png;base64," + this.state.product.image} />
+          <h5 className='text-center font-weight-bold '>{this.state.product.name}</h5>
+          <hr />
+          <p>{this.state.product.description}</p>
+        </div>
+
+        <div className='greybox z-depth-5 border border-light rounded'>
+
+          <h6 className='text-center'>Propriedade</h6>
+          <h5 className='text-center font-weight-bold '>{this.state.owner.fullname}</h5>
+
+          <div className='text-center'>
+            <a><img width="100px" height="100px" src={"data:image/png;base64," + this.state.owner.avatar} alt='profile_photo' className="rounded-circle" /></a><br />
+          </div>
+          <hr />
+          <p1>Usuário: {this.state.owner.name}</p1><br />
+          <p1>CNPJ/CPF: {this.state.owner.cpfcnpj}</p1><br />
+          <p1>E-mail: {this.state.owner.email}</p1><br />
+          <hr />
+          <div className='text-center'><Button href="/historico" size="sm">Histórico</Button></div>
+        </div>
+      </div >
+    );
+  }
+}
+
 export default Mainapp;
--- src/Menu.js
+++ src/Menu.js
@@ -12,55 +12,55 @@ import logo from './logo.png'
 class FullPageIntroWithFixedTransparentNavbar extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        collapse: false,
-        isWideEnough: false,
-      };
-  this.onClick = this.onClick.bind(this);
-}
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-onClick(){
-  this.setState({
-    collapse: !this.state.collapse,
-  });
-}
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
 
-render() {
-  return (
-    <div className='paddingtop'>
-    <BrowserRouter>
-        <div >
-              <Navbar  color="transparent" dark expand="md" fixed="top" scrolling>
-              
-                <NavbarBrand >
-                <img width="25px" src="https://www.ochain.com.br/static/media/logo.73a32887.png"/>
+  render() {
+    return (
+      <div className='paddingtop'>
+        <BrowserRouter>
+          <div >
+            <Navbar color="transparent" dark expand="md" fixed="top" scrolling>
+
+              <NavbarBrand >
+                <img width="25px" src="https://www.ochain.com.br/static/media/logo.73a32887.png" />
                 <strong>  Ochain</strong>
-                </NavbarBrand>
-                { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
-                <Collapse isOpen = { this.state.collapse } navbar>
-                  <NavbarNav right>
+              </NavbarBrand>
+              {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+              <Collapse isOpen={this.state.collapse} navbar>
+                <NavbarNav right>
 
-                    <NavItem className='textcenter' active>
-                        <NavLink onClick={this.onClick} to="/produto">Produto</NavLink>
-                    </NavItem>
+                  <NavItem className='textcenter' active>
+                    <NavLink onClick={this.onClick} to="/produto">Produto</NavLink>
+                  </NavItem>
 
-                    <NavItem className='textcenter' active>
-                        <NavLink onClick={this.onClick} to="/historico">Histórico</NavLink>
-                    </NavItem>
+                  <NavItem className='textcenter' active>
+                    <NavLink onClick={this.onClick} to="/historico">Histórico</NavLink>
+                  </NavItem>
 
-                  </NavbarNav>
-                  
-                </Collapse>
-              </Navbar>
+                </NavbarNav>
 
-              <div>
-              <Route path='/produto' component={Mainapp}/>
-              <Route path='/historico' component={Hist}/>
-              </div>
-              
-        </div>
+              </Collapse>
+            </Navbar>
+
+            <div>
+              <Route path='/produto/:code' component={Mainapp} />
+              <Route path='/historico' component={Hist} />
+            </div>
+
+          </div>
         </BrowserRouter>
-        </div>
+      </div>
     );
   }
 }
